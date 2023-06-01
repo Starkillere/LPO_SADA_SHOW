7d7e0487093ebbdc94515b816499441314dd272f
Ayouba Anrezki <anrezkia531@gmail.com>
2023-06-01 23:23:10 +0200

    fix problem of user ident en rsearch

--- config.py
+++ config.py
@@ -2,16 +2,16 @@
 
 import os
 
-SECRET_KEY = "#ak@3j^7wbw_y32zupc06!$3#u!0fo^oyty4ngp0)e#04-vdi9"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 MAIL_USERNAME = "lposada.show@outlook.fr"
-MAIL_PASSWORD = "d2_TbC?6Dq_UM2582z6F{db+3ez~R=J_"
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 MAIL_SERVER =  "smtp-mail.outlook.com"
 MAIL_PORT =  587
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 
-ADMINISTRATEUR_PASSWORD = "/6Xnnn_3P//z20rEAkr@kk/r5A%ZXeE@"
+ADMINISTRATEUR_PASSWORD = os.getenv("ADMINISTRATEUR_PASSWORD")
 ADMINISTRATEUR_EMAIL = "adm.lposadashow@gmail.com"
 ADMINISTRATEUR_NOM = "Administrateur"
 ADMINISTRATEUR_PRENOM = "Administrateur"
